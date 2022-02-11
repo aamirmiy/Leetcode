f224b37bfb7622a62533709fca932eb7cdf0e7d6
Aamir Miyajiwala <aamir.miyajiwala@gmail.com>
2022-02-11 14:02:24 +0530

    Time: 15 ms (56.64%), Space: 7.8 MB (53.44%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,36 +3,31 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> map1= {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        //map<char, int> map1;
-        // map1['I']=1;
-        // map1['V']=5;
-        // map1['X']=10;
-        // map1['L']=50;
-        // map1['C']=100;
-        // map1['D']=500;
-        // map1['M']=1000;
         
-        int temp=map1.at(s[s.length()-1]);
-        int sum=map1.at(s[s.length()-1]);
-        for(int i=s.length()-1;i>0;--i)
+        int arr[s.length()];
+        int sum=0;
+        //int temp=map1.at(s[s.length()-1]);
+        //int sum=map1.at(s[s.length()-1]);
+        //for(int i=s.length()-1;i>0;--i)
+        for(int i=0;i<s.length()-1;i++)
         {
-            if(temp<=map1.at(s[i-1]))
-            //if(map1.at(s[i])>=map1.at(s[i+1]))
+            //if(temp<=map1.at(s[i-1]))
+            if(map1.at(s[i])>=map1.at(s[i+1]))
             {
-                temp=map1.at(s[i-1]);
-               sum+=temp;
-                //s[i]=map1.at(s[i]);
+                //temp=map1.at(s[i-1]);
+               //sum+=temp;
+                arr[i]=map1.at(s[i]);
             }
             else{
-                sum-=map1.at(s[i-1]);
-                //s[i]=-map1.at(s[i]);
+                //sum-=map1.at(s[i-1]);
+                arr[i]=-map1.at(s[i]);
             }
         }
-        // s[s.length()-1]=map1.at(s[s.length()-1]);
-        // for(int j=0;j<s.length();j++)
-        // {
-        //     sum+=s[j];
-        // }
+         arr[s.length()-1]=map1.at(s[s.length()-1]);
+         for(int j=0;j<s.length();j++)
+         {
+             sum+=arr[j];
+         }
         return sum;
     }
 };
