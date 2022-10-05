818c770badbe3bd19fa4960604f7c996cf1eea5e
Aamir Miyajiwala <aamir.miyajiwala@gmail.com>
2022-10-05 14:15:54 -0700

    Time: 22 ms (28.92%), Space: 10.9 MB (18.05%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& rooms, unordered_map<int, bool> &vis, bool &ans, vector<int> &nodes, int &count)
+    void dfs(int node, vector<vector<int>>& rooms, unordered_map<int, bool> &vis, bool &ans, int &count)
     {
         
         vis[node]=true;
         count++;
-        nodes.push_back(node);
-        if(nodes.size()==rooms.size())
+        if(count==rooms.size())
         {
             ans= true;
         }
-        // else if(nodes.size()!= rooms.size() && count == rooms.size())
-        // {
-        //     ans = false;
-        // }
         else
         {
         for(auto x: rooms[node])
         {
             if(!vis[x])    
-                dfs(x, rooms, vis, ans, nodes, count);
+                dfs(x, rooms, vis, ans, count);
             else
                 continue;
         }
@@ -30,9 +25,8 @@ public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         bool ans;
         int count=0;
-        vector<int> nodes;
         unordered_map<int, bool> vis;
-        dfs(0, rooms, vis, ans, nodes, count);
+        dfs(0, rooms, vis, ans, count);
         return ans;
     }
 };
