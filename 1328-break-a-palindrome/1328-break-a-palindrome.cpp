790ca04b489ca7fc29e8e716c2bbdb18ea5cfd5a
Aamir Miyajiwala <aamir.miyajiwala@gmail.com>
2022-10-17 23:28:35 -0700

    Time: 5 ms (19.89%), Space: 6.1 MB (94.89%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        int n = palindrome.length();
+        if(n==1||n==0)
+            return "";
+        for (int i=0;i<n/2;i++)
+        {
+            if(palindrome[i]!='a')
+            {
+                palindrome[i]='a';
+                return palindrome;
+            }
+        }
+        palindrome[n-1]='b';
+        return palindrome;
+    }
+};
