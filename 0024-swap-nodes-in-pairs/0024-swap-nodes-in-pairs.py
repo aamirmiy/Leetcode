219e62821f36f7f819caa4d9121d6a70f971701c
Aamir Miyajiwala <aamir.miyajiwala@gmail.com>
2022-10-29 19:09:53 -0700

    Time: 54 ms (62.54%), Space: 13.9 MB (19.23%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -7,15 +7,15 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy=prev = ListNode(0) #dummy node
         prev.next = head
-        while head and head.next:
+        while head and head.next: #condition to check pairs
             a= head
             b= a.next
             c= b.next
-            
+            #reversing pair
             prev.next=b
             b.next=a
             a.next=c
-        
+        #updating pointers
             prev=a
             head=c
         return dummy.next
